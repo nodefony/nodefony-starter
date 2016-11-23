4973e1f8464a69bcfe2c2da0fee8f37a219eaab6
cci <ccamensuli@gmail.com>
2016-11-23 17:39:43 +0100

    travis try debug env variable

--- .travis/bin/install.sh
+++ .travis/bin/install.sh
@@ -2,6 +2,8 @@
 
 make clean ;
 
+echo "INSTALL NODEFONY TRAVIS ENVIRONEMENT $DB ACTIVE ";
+
 if [ "$DB" = "mysql" ]
 	echo "NODEFONY TRAVIS ENVIRONEMENT MYSQL ACTIVE " ;
 	cp .travis/config/config.yml app/config/config.yml ;
