b55ee82a6ed6e11d2a14e141659e8e724aff9e29
cci <ccamensuli@gmail.com>
2016-11-23 17:46:05 +0100

    debug bash install.sh travis

--- .travis/bin/install.sh
+++ .travis/bin/install.sh
@@ -5,9 +5,10 @@ make clean ;
 echo "INSTALL NODEFONY TRAVIS ENVIRONEMENT $DB ACTIVE ";
 
 if [ "$DB" = "mysql" ]
+then
 	echo "NODEFONY TRAVIS ENVIRONEMENT MYSQL ACTIVE " ;
 	cp .travis/config/config.yml app/config/config.yml ;
-then
+else
 	echo "NODEFONY TRAVIS ENVIRONEMENT SQLITE ACTIVE " ;
 fi
 
