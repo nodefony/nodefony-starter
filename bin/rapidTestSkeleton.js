37c220a193e7be0cc68f3d8766b9c49d34719310
cci <ccamensuli@gmail.com>
2016-11-23 18:17:30 +0100

    change travis var

---
+++ bin/rapidTestSkeleton.js
@@ -0,0 +1,16 @@
+#!/usr/local/bin/node  
+
+
+// AUTOLOADER 
+var autoloader = require("../vendors/nodefony/core/autoloader");
+
+
+
+// HERE NaneSpace nodefony is ready 
+
+
+
+// CREATE INSTANCE OF CONTAINER 
+var globalContainer  = new nodefony.Container() ;
+
+
