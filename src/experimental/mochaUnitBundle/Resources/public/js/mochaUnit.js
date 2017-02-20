8e62bb28f6d889edce53237a68dbfb02497470c6
cci <ccamensuli@gmail.com>
2017-02-20 14:39:24 +0100

    delete experimental directory

--- src/experimental/mochaUnitBundle/Resources/public/js/mochaUnit.js
+++
@@ -1,8 +0,0 @@
-/**
- * New node file
- */
-
-var depends = function depends(libPath){
-	console.log('LOADING : ' + libPath);
-	$('body').append($('<script type="text/javascript" src="' + libPath + '?time=' + (new Date()).getTime() + '"></script>'));
-};
